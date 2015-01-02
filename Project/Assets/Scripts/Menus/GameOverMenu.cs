96fb4489e4f7cc801aeb87efb28deb3e729f0b99
Benom84 <menahem.ben@gmail.com>
2015-01-03 01:32:58 +0200

    AddedRestartToGameOver

---
+++ Project/Assets/Scripts/Menus/GameOverMenu.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameOverMenu : MonoBehaviour {
+	public enum GameOverButton {Restart, MainMenu}
+	public GameOverButton button;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (Input.GetKeyDown ("space"))
+						Application.LoadLevel ("GrassLevel2");
+	
+	}
+}
