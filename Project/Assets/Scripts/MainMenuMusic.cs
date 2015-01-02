1a31534af9ee8114d077f12d9f51d4743b9cf602
Benom84 <menahem.ben@gmail.com>
2015-01-02 10:05:40 +0200

    Initial

---
+++ Project/Assets/Scripts/MainMenuMusic.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MainMenuMusic : MonoBehaviour {
+
+	void Awake () {
+		GameObject gameMusic = GameObject.Find("GameMusic");
+		if (gameMusic) {
+			Destroy(gameMusic);
+		}
+		DontDestroyOnLoad(gameObject);
+		if (FindObjectsOfType(GetType()).Length > 1)
+		{
+			Destroy(gameObject);
+		}
+	}
+
+}
---
+++ Project/Assets/Scripts/MainMenuScript.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+
+
+public class MainMenuScript : MonoBehaviour {
+
+	[HideInInspector]
+	public enum Button{Quit, Sound, Play, About}
+
+	public Button button;
+
+	void Start() {
+
+		if (button == Button.Sound)
+				if (!PlayerPrefs.HasKey ("Music")) {
+						PlayerPrefs.SetString ("Music", "On");
+						audio.mute = false;
+				}
+
+	}
+
+	void OnMouseDown() {
+		if (button == Button.Quit)
+			Application.Quit ();
+		else if (button == Button.Play)
+			Application.LoadLevel ("GrassLevel2");
+		else if (button == Button.About)
+			Application.LoadLevel ("About");
+		else if (PlayerPrefs.GetString("Music") == "On") {
+			PlayerPrefs.SetString ("Music", "Off");
+			audio.mute = true;
+		} else {
+			PlayerPrefs.SetString ("Music", "On");
+			audio.mute = false;
+		}
+	}
+
+}
