b70e30a343bb7f8df3bcae3c0013f0978bcf7882
Benom84 <menahem.ben@gmail.com>
2015-01-04 11:46:53 +0200

    ClosingScene?

---
+++ Project/Assets/Scripts/ClosingSceneScript.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ClosingSceneScript : MonoBehaviour {
+
+	public float zStart;
+	public float zColorChange;
+	public float zEnd;
+	public float change;
+
+	private int direction = 1;
+	private Vector3 vChange;
+	private Light light;
+	private bool changedLight;
+
+	// Use this for initialization
+	void Start () {
+
+		Vector3 pos =  new Vector3 (transform.position.x, transform.position.y, zStart);
+		vChange = new Vector3 (0, 0, change);
+		light = transform.FindChild ("MovieLight").GetComponent<Light>();
+		transform.position = pos;
+
+		if (zEnd < zStart)
+						direction = -1;
+	
+	}
+	
+
+	void Update () {
+
+		if (transform.position.z < zEnd) {
+			if (transform.position.z > zColorChange)
+				light.color += Color.red / 2.0F * Time.deltaTime;
+			Vector3 newPos = transform.position;
+			newPos = newPos + vChange;
+			transform.position = newPos;
+		}
+
+	
+	
+	}
+}
