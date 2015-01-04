13e1760b3472da2d3f79f5808968d2a55e06532d
Benom84 <menahem.ben@gmail.com>
2015-01-04 12:40:14 +0200

    ClosingScene2?

--- Project/Assets/Scripts/ClosingSceneScript.cs
+++ Project/Assets/Scripts/ClosingSceneScript.cs
@@ -11,7 +11,8 @@ public class ClosingSceneScript : MonoBehaviour {
 	private int direction = 1;
 	private Vector3 vChange;
 	private Light light;
-	private bool changedLight;
+	private SpriteRenderer message;
+	private AudioSource listener;
 
 	// Use this for initialization
 	void Start () {
@@ -19,6 +20,9 @@ public class ClosingSceneScript : MonoBehaviour {
 		Vector3 pos =  new Vector3 (transform.position.x, transform.position.y, zStart);
 		vChange = new Vector3 (0, 0, change);
 		light = transform.FindChild ("MovieLight").GetComponent<Light>();
+		message = transform.FindChild ("YouWon").GetComponent<SpriteRenderer>();
+		listener = GetComponent<AudioSource> ();
+		message.color = new Color (1f, 1f, 1f, 0f);
 		transform.position = pos;
 
 		if (zEnd < zStart)
@@ -32,12 +36,24 @@ public class ClosingSceneScript : MonoBehaviour {
 		if (transform.position.z < zEnd) {
 			if (transform.position.z > zColorChange)
 				light.color += Color.red / 2.0F * Time.deltaTime;
+			if (transform.position.z > zColorChange) {
+				message.color = new Color (1f, 1f, 1f, 1f);
+				Debug.Log ("Should be visible");
+			}
+				
 			Vector3 newPos = transform.position;
 			newPos = newPos + vChange;
 			transform.position = newPos;
 		}
 
+		if (!listener.isPlaying)
+						Application.LoadLevel ("MainMenu");
 	
-	
+	}
+
+	void OnMouseDown() {
+
+		Application.LoadLevel ("MainMenu");
+
 	}
 }
